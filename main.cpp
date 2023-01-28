670cd2dbc22de6a170fe06cf2d5b9e41e3101576
MisterB <barradecompas@gmail.com>
2023-01-27 20:04:42 -0500

    Capitulo 1 PPP con cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,8 @@
-// This program outputs the message “Hello, World!” to the monitor
 #include "std_lib_facilities.h"
-int main() // C++ programs start by executing the function main
+int main()
 {
-    cout << "Hello, World!\n"; // output “Hello, World!”
+    cout << "Hello, programing\n";
+    cout << "Here we go!\n";
+    keep_window_open();
     return 0;
 }
