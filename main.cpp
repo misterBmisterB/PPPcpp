7cafcc8261de9f775ad1a40b2b128b2542246f38
MisterB <barradecompas@gmail.com>
2023-02-05 22:46:00 -0500

    Try This 4.4.2.1

--- main.cpp
+++ main.cpp
@@ -1,16 +1,10 @@
 /* Felipe Bonilla 2023
 Programming/ Principles and Practice Using C++.pdf
+
 */
 #include "std_lib_facilities.h"
-void use(int n)
-{
-    constexpr int c1 = max + 7;
-    const int c2 = n + 7;
-    // ...
-    cout << "Número:\n";
-    cin >> c2;
-};
+
 int main()
 {
-    constexpr unsigned int max = 100;
+
 }
