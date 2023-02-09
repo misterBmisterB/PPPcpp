c54f6f9c0268a3832722c5d2fdce027f2f85d21f
Felipe Bonilla <barradecompas@gmail.com>
2023-02-08 22:10:34 -0500

    Actualizacion

--- main.cpp
+++ main.cpp
@@ -1,10 +1,22 @@
 /* Felipe Bonilla 2023
 Programming/ Principles and Practice Using C++.pdf
-
+Try This
 */
 #include "std_lib_facilities.h"
+
+int al_cuadrado(int n)
+{
+    int m{0};
+    for (int i{0}; i <= n; ++i)
+    {
+        m = n + n;
+    }
+    return m;
+}
 
 int main()
 {
 
+    for (int i = 0; i < 100; ++i)
+        cout << i << '\t' << al_cuadrado(i) << '\n';
 }
