25d32728a1d3c0a464bd9b1e2560cb200cbdaf05
Felipe Bonilla <barradecompas@gmail.com>
2023-02-08 22:38:22 -0500

    Listo en capitulo Vector

--- main.cpp
+++ main.cpp
@@ -1,19 +1,8 @@
 /* Felipe Bonilla 2023
 Programming/ Principles and Practice Using C++.pdf
-Try This
 */
 #include "std_lib_facilities.h"
 
-int al_cuadrado(int n)
-{
-    int m{n};
-    for (int i = 1; i < n; ++i)
-        m = m + n;
-    return m;
-}
-
 int main()
 {
-    for (int i = 0; i < 100; ++i)
-        cout << i << '\t' << al_cuadrado(i) << '\n';
 }
