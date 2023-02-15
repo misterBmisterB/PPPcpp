1fa1b09dd19b18bdbf16fe96777d918114517287
Felipe Bonilla <barradecompas@gmail.com>
2023-02-14 22:55:22 -0500

    Drill P1C4P6

--- main.cpp
+++ main.cpp
@@ -5,4 +5,33 @@ Programming/ Principles and Practice Using C++.pdf
 
 int main()
 {
+    vector<double> enteros;
+    double doble_ingresado, menor, mayor;
+    while (cin >> doble_ingresado)
+    {
+        enteros.push_back(doble_ingresado);
+        if (enteros.size() == 1)
+        {
+            cout << "Este es el único valor ingresado: " << enteros[0] << endl;
+            menor = doble_ingresado;
+            mayor = doble_ingresado;
+        }
+        else if (doble_ingresado < menor)
+        {
+            cout << "Este es el valor menor ingresado hasta ahora: " << doble_ingresado << endl;
+            menor = doble_ingresado;
+        }
+        else if (doble_ingresado > mayor)
+        {
+            cout << "Este es el valor mayor ingresado hasta ahora: " << doble_ingresado << endl;
+            mayor = doble_ingresado;
+        }
+        else
+        {
+            cout << "Este es el valor ingresado: " << doble_ingresado << endl;
+        }
+    }
+    cout << "While terminado\n";
+    sort(enteros);
+    cout << "El menor es: " << enteros[0] << ", y el mayor es: " << enteros[enteros.size()-1] << endl;
 }
