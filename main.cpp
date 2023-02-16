42ddeb98422f848d489fb5399e0eac79546284fc
Felipe Bonilla <122578665+misterBmisterB@users.noreply.github.com>
2023-02-16 18:20:36 +0000

    Drill 4.7

--- main.cpp
+++ main.cpp
@@ -1,37 +1,80 @@
 /* Felipe Bonilla 2023
 Programming/ Principles and Practice Using C++.pdf
+Drill 4.7
 */
 #include "std_lib_facilities.h"
 
 int main()
 {
-    vector<double> enteros;
-    double doble_ingresado, menor, mayor;
-    while (cin >> doble_ingresado)
+    vector<double> valores;
+    double doble_ingresado, menor, mayor, total{0};
+    constexpr double cms_a_ms{0.01}, pulg_a_ms{2.54 * cms_a_ms}, pies_a_ms{12 * pulg_a_ms};
+    int indice{0}, indice_menor{indice}, indice_mayor{indice};
+    string unidad;
+    while (cin >> doble_ingresado >> unidad)
     {
-        enteros.push_back(doble_ingresado);
-        if (enteros.size() == 1)
+        double doble_en_ms;
+        if (unidad == "cm" || unidad == "cms")
         {
-            cout << "Este es el único valor ingresado: " << enteros[0] << endl;
-            menor = doble_ingresado;
-            mayor = doble_ingresado;
+            doble_en_ms = doble_ingresado * cms_a_ms;
+            cout << "Ingresaste: " << doble_ingresado << unidad << ", que son " << doble_en_ms << "m.\n";
         }
-        else if (doble_ingresado < menor)
+        else if (unidad == "in" || unidad == "inches" || unidad == "pulg" || unidad == "pulgadas")
         {
-            cout << "Este es el valor menor ingresado hasta ahora: " << doble_ingresado << endl;
-            menor = doble_ingresado;
-        }
-        else if (doble_ingresado > mayor)
+            doble_en_ms = doble_ingresado * pulg_a_ms;
+cout << "Ingresaste: " << doble_ingresado << unidad << ", que son " << doble_en_ms << "m.\n";        }
+        else if (unidad == "pie" || unidad == "pies" || unidad == "ft")
         {
-            cout << "Este es el valor mayor ingresado hasta ahora: " << doble_ingresado << endl;
-            mayor = doble_ingresado;
+            doble_en_ms = doble_ingresado * pies_a_ms;
+cout << "Ingresaste: " << doble_ingresado << unidad << ", que son " << doble_en_ms << "m.\n";        }
+        else if (unidad == "m" || unidad == "ms" || unidad == "metros")
+        {
+            doble_en_ms = doble_ingresado;
+            cout << "Ingresaste: " << doble_ingresado << unidad << endl;
         }
         else
         {
-            cout << "Este es el valor ingresado: " << doble_ingresado << endl;
+            cout << "valor inválido\n";
         }
+        total += doble_en_ms;
+        valores.push_back(doble_en_ms);
+        if (valores.size() == 1)
+        {
+            menor = doble_en_ms;
+            mayor = doble_en_ms;
+            indice_menor = indice;
+            indice_mayor = indice;
+        }
+        else if (doble_en_ms < menor)
+        {
+            menor = doble_en_ms;
+            cout << "Este es el valor menor ingresado hasta ahora: " << menor << endl;
+            
+            indice_menor = indice;
+        }
+        else if (doble_en_ms > mayor)
+        {
+            mayor = doble_en_ms;
+            cout << "Este es el valor mayor ingresado hasta ahora: " << mayor << endl;
+            
+            indice_mayor = indice;
+        }
+        else
+        {
+           
+        }
+        indice++;
     }
     cout << "While terminado\n";
-    sort(enteros);
-    cout << "El menor es: " << enteros[0] << ", y el mayor es: " << enteros[enteros.size()-1] << endl;
+    cout << "La suma total en metros es de: " << total << " m.\n";
+    cout << "El valor menor ingresado, en metros, fue: " << menor << " m.\n";
+    cout << "El valor mayor ingresado, en metros, fue: " << mayor << " m.\n";
+    cout << "El número de valores ingresados fue: " << indice << ".\n";
+    sort(valores);
+    cout << "Los valores ingresados fueron, en metros, de menor a mayor: ";
+    for (double i : valores)
+    {
+        cout << i << "m ";
+    }
+    cout << ".\n";
 }
